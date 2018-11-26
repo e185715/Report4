e90ce604231b9275e581fba00bad4a1407414e57
Shinto Higa <e185715@ie.u-ryukyu.ac.jp>
2018-11-27 00:12:50 +0900

    レポート4 Step3のコミット

--- src/jp/ac/uryukyu/ie/e185715/Main.java
+++ src/jp/ac/uryukyu/ie/e185715/Main.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            String str = null;
+            String str = "百二十三";
+            int i = Integer.parseInt(str);
+            int value;
             System.out.println(str.length());
         }catch (NullPointerException e){
             System.out.println("! NullPointerExceptionだお !");
